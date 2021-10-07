d31f225f10a72ef39820898ca0dcf9326bf9e3a1
dhenwood <dhenwood@users.noreply.github.com>
2021-10-07 17:49:23 +1100

    Create KioskEscalate.js

---
+++ KioskEscalate.js
@@ -0,0 +1,52 @@
+import xapi from 'xapi';
+
+var deviceId;
+
+xapi.event.on('UserInterface Extensions Panel Clicked', (event) => {
+  if(event.PanelId == 'escalateQantas'){
+    xapi.command("UserInterface WebView Display", {Title: "Options", Url: "https://talk2spark.com/qantasKiosk/index.html?deviceId=" + deviceId})
+  }else if(event.PanelId == 'escalateNrmaEn'){
+    xapi.command("UserInterface WebView Display", {Title: "Options", Url: "https://talk2spark.com/iagKiosk/index.html?deviceId=" + deviceId})
+  }else if(event.PanelId == 'escalateNrmaVn'){
+    xapi.command("UserInterface WebView Display", {Title: "Options", Url: "https://talk2spark.com/iagKiosk/indexVn.html?deviceId=" + deviceId})
+  }
+});
+
+// Prevent audio volume from changing (also noise removal, mute, external things, also power off, etc)
+// https://github.com/alvulovi/VideoKiosk/blob/main/VideoKiosk.js 
+
+function init(){
+  xapi.status.get('SystemUnit Hardware Module SerialNumber').then ((value) => {
+    console.log("Serial Number: " + value);
+    getDevice(value);
+  })
+}
+
+
+function getDevice(serialNumber){
+  // Need to get deviceId  ** USING TEMP TOKEN RIGHT NOW (need to change to BOT) **
+  const TOKEN = "<insert Token>";
+  const BEARERTOKEN = "Authorization: Bearer " + TOKEN;
+  const WEBEXDEVICEURL = 'https://webexapis.com/v1/devices?serial=' + serialNumber; 
+  const CONTENT_TYPE = "Content-Type: application/json";
+  
+  xapi.command('HttpClient Get', { 'Header': [CONTENT_TYPE, BEARERTOKEN] , 'Url':WEBEXDEVICEURL, 'AllowInsecureHTTPS': 'True'})
+  .then(
+    (result) => {
+       //if (response.StatusCode == 200) {
+      var code = result.StatusCode;
+      console.log("code: " + code);
+      var body = result.Body;
+      var totalItems = JSON.parse(body).items.length
+      if(totalItems > 0){
+        deviceId =  JSON.parse(body).items[0].id;
+        console.log("id: " + deviceId);
+      }else{
+        console.log("Error: Unable to find deviceId from serial number: " + serialNumber);
+      }
+
+    }
+  );
+}
+
+init();
